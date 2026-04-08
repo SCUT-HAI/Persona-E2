233984da311f16142f65c63039e34231191eaf97
Yang <113090727+CRIS-YANGYQ@users.noreply.github.com>
2026-04-08 12:17:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/Persona-E2/',
+  base:'/Persona-E2',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
